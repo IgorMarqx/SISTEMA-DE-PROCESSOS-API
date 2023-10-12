78a7d132a325856a1c45ee07d6531954ef05a437
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-11 21:49:12 -0300

    feat: implement new routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\api\auth\AuthController;
 
 Route::get('/ping', function () {
     return ['pong' => true];
+});
+
+Route::get('unauthorized', function () {
+   return response()->json(['error' => 'Unauthorized'], 401);
+})->name('api.unauthorized');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['api.auth'])->group(function () {
+
+    Route::apiResources([
+
+    ]);
 });
