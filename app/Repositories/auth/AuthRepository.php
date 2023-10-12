0be279527c0a2872e6f0d4faa0dcb57ad7eba1a1
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-12 10:10:07 -0300

    infra: format code with pint

--- app/Repositories/auth/AuthRepository.php
+++ app/Repositories/auth/AuthRepository.php
@@ -9,8 +9,8 @@ class AuthRepository implements AuthRepositoryInterface
     public function login($data): string
     {
         return Auth::attempt([
-            'email' => $data->email,
-            'password' => $data->password
+            'email'    => $data->email,
+            'password' => $data->password,
         ]);
     }
 }
