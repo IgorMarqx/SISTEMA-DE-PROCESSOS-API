d077343c3d58bd0338c334e7ea4639c30b4fd47c
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-11 21:49:35 -0300

    config: register repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\auth\AuthRepository;
+use App\Repositories\auth\AuthRepositoryInterface;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(AuthRepositoryInterface::class, AuthRepository::class);
     }
 
     /**
