b9572553b3d3225600b1efe198c9b78624aa60b1
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-12 10:07:07 -0300

    feat: create new routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Http\Controllers\api\auth\AuthController;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\api\auth\AuthController;
+use \App\Http\Controllers\api\process\CollectiveController;
+use \App\Http\Controllers\api\process\IndividualController;
 
 Route::get('/ping', function () {
     return ['pong' => true];
 });
 
 Route::get('unauthorized', function () {
-   return response()->json(['error' => 'Unauthorized'], 401);
+    return response()->json(['error' => 'Unauthorized'], 401);
 })->name('api.unauthorized');
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -16,6 +18,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['api.auth'])->group(function () {
 
     Route::apiResources([
-
+        'collective' => CollectiveController::class,
+        'individual' => IndividualController::class
     ]);
 });
