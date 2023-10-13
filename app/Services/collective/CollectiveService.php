8d5d2b0a53787c92f9a42b10d24752362e2e38e3
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-13 20:18:45 -0300

    feat: implement new logics

--- app/Services/collective/CollectiveService.php
+++ app/Services/collective/CollectiveService.php
@@ -2,7 +2,51 @@
 
 namespace App\Services\collective;
 
+use App\Models\Collective;
+use App\Repositories\collective\CollectiveRepositoryInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 class CollectiveService
 {
+    protected CollectiveRepositoryInterface $collectiveRepository;
 
+    public function __construct(CollectiveRepositoryInterface $collectiveRepository)
+    {
+        $this->collectiveRepository = $collectiveRepository;
+    }
+
+    public function getAll(): LengthAwarePaginator
+    {
+        return $this->collectiveRepository->getAll();
+    }
+
+    public function createCollective($data): Collective
+    {
+        return $this->collectiveRepository->createCollective($data);
+    }
+
+    public function getCollectiveById($id): bool|Collective
+    {
+        return $this->collectiveRepository->getCollectiveById($id);
+    }
+
+    public function finishedCollective($id): bool
+    {
+        $collective = $this->getCollectiveById($id);
+
+        if (!$collective) {
+            return false;
+        }
+
+        if($collective->finish && $collective->progress === 0) {
+            return false;
+        }
+
+        return $this->collectiveRepository->finishedCollective($collective, [
+            'progress'   => 0,
+            'finish'     => 1,
+            'qtd_finish' => $collective->qtd_finish + 1,
+            'updated_at' => now(),
+        ]);
+    }
 }
