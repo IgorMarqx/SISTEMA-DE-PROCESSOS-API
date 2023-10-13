f5ef88c24fceb97e65f86b28ecbb0057afe57e7b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-13 20:18:17 -0300

    feat: implement new insertions

--- app/Repositories/collective/CollectiveRepository.php
+++ app/Repositories/collective/CollectiveRepository.php
@@ -2,6 +2,62 @@
 
 namespace App\Repositories\collective;
 
-class CollectiveRepository
+use App\Models\Collective;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class CollectiveRepository implements CollectiveRepositoryInterface
 {
+    public function getAll(): LengthAwarePaginator
+    {
+        return Collective::paginate(5);
+    }
+
+    public function createCollective($data): Collective
+    {
+        return Collective::create([
+            'name'               => $data['name'],
+            'user_id'            => $data['user_id'],
+            'lawyer_id'          => $data['lawyer_id'],
+            'subject'            => $data['subject'],
+            'jurisdiction'       => $data['jurisdiction'],
+            'cause_value'        => $data['cause_value'],
+            'justice_secret'     => $data['justice_secret'],
+            'free_justice'       => $data['free_justice'],
+            'tutelary'           => $data['tutelary'],
+            'priority'           => $data['priority'],
+            'judgmental_organ'   => $data['judgmental_organ'],
+            'judicial_office'    => $data['judicial_office'],
+            'competence'         => $data['competence'],
+            'url_collective'     => $data['url_collective'],
+            'url_notices'        => $data['url_notices'],
+            'email_coorporative' => $data['email_coorporative'],
+            'email_client'       => $data['email_client'],
+            'progress'           => 1,
+        ]);
+    }
+
+    public function getCollectiveById($id): bool|Collective
+    {
+        return Collective::find($id);
+    }
+
+    public function updateCollective($data)
+    {
+        // TODO: Implement updateCollective() method.
+    }
+
+    public function finishedCollective(Collective $collective, $data): bool
+    {
+        return $collective->update($data);
+    }
+
+    public function deleteCollective()
+    {
+        // TODO: Implement deleteCollective() method.
+    }
+
+    public function filterCollective($data)
+    {
+        // TODO: Implement filterCollective() method.
+    }
 }
