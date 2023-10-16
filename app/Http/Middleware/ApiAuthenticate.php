687b8dbdfbb32dd12e138e74f037ec50c8668b04
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-16 17:26:21 -0300

    config: middleware to unauthorized

---
+++ app/Http/Middleware/ApiAuthenticate.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class ApiAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+
+            if (!$user) {
+                return redirect()->route('api.unauthorized');
+            }
+        } catch (\Exception $e) {
+            return redirect()->route('api.unauthorized');
+        }
+
+        return $next($request);
+    }
+}
