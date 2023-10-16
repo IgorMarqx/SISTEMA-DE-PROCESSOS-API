af9971e827863dce9a5d686002a427d17198a263
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-16 18:09:32 -0300

    config: refactor the name

--- app/Models/Process.php
+++ app/Models/Process.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Collective extends Model
+class Process extends Model
 {
     use HasFactory;
 
@@ -32,5 +32,6 @@ class Collective extends Model
         'progress',
         'finish',
         'update',
+        'type_process',
     ];
 }
