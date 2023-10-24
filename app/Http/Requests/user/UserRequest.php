856d36bf6fc0d4a08f03b3e19238177bbaab4194
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-24 20:47:01 -0300

    feat: implement create user and delete

--- app/Http/Requests/user/UserRequest.php
+++ app/Http/Requests/user/UserRequest.php
@@ -2,18 +2,13 @@
 
 namespace App\Http\Requests\user;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class UserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -22,7 +17,28 @@ class UserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'      => ['required', 'string', 'max:255'],
+            'email'     => ['required', 'email', 'max:255', 'unique:users,email'],
+            'password'  => ['required', 'min:5'],
+            'admin'     => ['required', 'string', 'max:255', Rule::in(['user', 'admin', 'lawyer'])],
+            'organ'     => ['nullable', 'string', 'max:255'],
+            'office'    => ['nullable', 'string', 'max:255'],
+            'capacity'  => ['nullable', 'string', 'max:255'],
+            'telephone' => ['nullable', 'string', 'max:255'],
+            'cpf'       => ['unique:users', 'string', 'max:255', 'min:11'],
+            'oab'       => ['nullable', 'string', 'max:255'],
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'error'      => true,
+            'message'    => $validator->errors()->first(),
+            'path'       => $this->fullUrl(),
+            'method'     => $this->getMethod(),
+            'timestamps' => now()->format('Y-m-d H:i:s'),
+            'status'     => 422,
+        ], 422));
     }
 }
