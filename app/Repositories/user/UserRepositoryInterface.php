ec2503c4079a18ee0957fe093999b2e61eed539e
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-24 20:46:53 -0300

    feat: implement create user and delete

--- app/Repositories/user/UserRepositoryInterface.php
+++ app/Repositories/user/UserRepositoryInterface.php
@@ -8,9 +8,9 @@ use Illuminate\Pagination\LengthAwarePaginator;
 interface UserRepositoryInterface
 {
     public function getAll(): LengthAwarePaginator;
-    public function createUser($data);
+    public function createUser($data): User;
     public function getUserById($id): User|null;
     public function updateUser(User $user, $data);
-    public function deleteUser(User $user);
+    public function deleteUser(User $user): bool;
     public function filterUser($data);
 }
