e1098e635462f7b0b00068cd6adb3c62dbc8d8e6
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-25 20:15:50 -0300

    feat: attachment factory

---
+++ database/factories/AttachmentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Process;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class AttachmentFactory extends Factory
+{
+    protected array $type_process = ['collective', 'individual'];
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title(),
+            'process_id' => Process::factory(),
+            'user_id' => User::factory(),
+            'type_process' => fake()->randomElement($this->type_process),
+            'path' => fake()->filePath(),
+        ];
+    }
+}
