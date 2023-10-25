59323b9ca86c6752bb71f56cdf9d42a9156c18d1
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-25 20:16:13 -0300

    feat: attachment repository

---
+++ app/Repositories/attachment/AttachmentRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repositories\attachment;
+
+class AttachmentRepository implements AttachmentRepositoryInterface
+{
+    public function uploadAttachment($request, $id)
+    {
+    }
+
+    public function deleteAttachment($id)
+    {
+    }
+
+    public function downloadAttachment($id)
+    {
+    }
+
+    public function getAttachment()
+    {
+    }
+}
