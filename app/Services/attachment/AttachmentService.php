d9cc121eaf5e5e206389bfebd357ccc6802ea007
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-26 20:23:52 -0300

    feat: implement attachment service

--- app/Services/attachment/AttachmentService.php
+++ app/Services/attachment/AttachmentService.php
@@ -2,7 +2,24 @@
 
 namespace App\Services\attachment;
 
+use App\Repositories\attachment\AttachmentRepositoryInterface;
+
 class AttachmentService
 {
+    private AttachmentRepositoryInterface $attachmentRepository;
+
+    public function __construct(AttachmentRepositoryInterface $attachmentRepository)
+    {
+        $this->attachmentRepository = $attachmentRepository;
+    }
+
+    public function uploadAttachment($request, $receivedFile)
+    {
+        $file = $this->attachmentRepository->uploadAttachment($request, $receivedFile);
+
+        $receivedFile->storeAs('public/attachments', $receivedFile->getClientOriginalName());
+
+        return $file;
+    }
 
 }
