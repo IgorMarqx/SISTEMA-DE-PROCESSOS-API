59b32f5ca9090e8ae7e30a6bee6424449ce87b25
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-26 20:23:01 -0300

    feat: implement attachment controller

--- app/Http/Controllers/api/attachment/AttachmentController.php
+++ app/Http/Controllers/api/attachment/AttachmentController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\api\attachment;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\attachment\AttachmentRequest;
+use App\Services\attachment\AttachmentService;
 use Illuminate\Http\Request;
 
 class AttachmentController extends Controller
 {
-    //
+    private AttachmentService $attachmentService;
+
+    public function __construct(AttachmentService $attachmentService)
+    {
+        $this->attachmentService = $attachmentService;
+    }
+
+    public function uploadAttachment(AttachmentRequest $request)
+    {
+        if($request->hasFile('file')){
+            $file = $request->file('file');
+            $this->attachmentService->uploadAttachment($request, $file);
+        }
+    }
 }
