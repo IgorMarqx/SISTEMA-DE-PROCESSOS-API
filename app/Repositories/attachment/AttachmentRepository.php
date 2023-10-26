3ec98e61b40e3084f3de70bceaacc940ea7cac30
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-26 20:23:28 -0300

    feat: implement attachment repository

--- app/Repositories/attachment/AttachmentRepository.php
+++ app/Repositories/attachment/AttachmentRepository.php
@@ -2,10 +2,21 @@
 
 namespace App\Repositories\attachment;
 
+use App\Models\Attachment;
+
 class AttachmentRepository implements AttachmentRepositoryInterface
 {
-    public function uploadAttachment($request, $id)
+    public function uploadAttachment($data, $receivedFile)
     {
+        return Attachment::create([
+            'title' => $receivedFile->getClientOriginalName(),
+            'process_id' => $data->process_id,
+            'user_id' => $data->user_id,
+            'type_process' => $data->type_process,
+            'path' => $receivedFile->path(),
+            'type' => $receivedFile->getClientOriginalExtension(),
+            'size' => $receivedFile->getSize(),
+        ]);
     }
 
     public function deleteAttachment($id)
