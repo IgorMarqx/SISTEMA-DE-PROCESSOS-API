d952a3c291e53f37c7627ea5798eb9cb5992b06f
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-10-29 12:54:52 -0300

    feat: implement attachment controller

--- app/Http/Controllers/api/attachment/AttachmentController.php
+++ app/Http/Controllers/api/attachment/AttachmentController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\api\attachment;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\attachment\AllAttachmentByProcessIdRequest;
 use App\Http\Requests\attachment\AttachmentRequest;
+use App\Http\Resources\GlobalResource;
 use App\Services\attachment\AttachmentService;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use function PHPUnit\Framework\isEmpty;
 
 class AttachmentController extends Controller
 {
@@ -16,11 +19,85 @@ class AttachmentController extends Controller
         $this->attachmentService = $attachmentService;
     }
 
-    public function uploadAttachment(AttachmentRequest $request)
+    /**
+     * @throws \Exception
+     */
+    public function uploadAttachment(AttachmentRequest $request): GlobalResource
     {
-        if($request->hasFile('file')){
-            $file = $request->file('file');
-            $this->attachmentService->uploadAttachment($request, $file);
+       $this->attachmentService->uploadAttachment($request, $request->file('file'));
+
+        try {
+            return new GlobalResource(['error' => false, 'message' => 'Successfully uploaded attachment'], 201);
+        }catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function getAttachmentId(string $id): GlobalResource
+    {
+        $attachment = $this->attachmentService->getAttachmentId($id);
+
+        if(!$attachment){
+            return new GlobalResource(['error' => true, 'message' => 'Attachment not found'], 404);
+        }
+
+        try {
+            return new GlobalResource(['error' => false, 'message' => $attachment], 200);
+        }catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function deleteAttachment(string $id): GlobalResource
+    {
+        $attachment = $this->attachmentService->deleteAttachment($id);
+
+        if(!$attachment){
+            return new GlobalResource(['error' => true, 'message' => 'Attachment not found'], 404);
+        }
+
+        try {
+            return new GlobalResource(['error' => false, 'message' => 'Successfully deleted attachment'], 200);
+        }catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function downloadAttachment(string $id): BinaryFileResponse|GlobalResource
+    {
+        $attachment = $this->attachmentService->getAttachmentId($id);
+
+        if(!$attachment){
+            return new GlobalResource(['error' => true, 'message' => 'Attachment not found'], 404);
+        }
+
+        try {
+            return response()->download($attachment->path. $attachment->title);
+        }catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function getAllAttachmentByProcessId(string $id): GlobalResource
+    {
+        $attachment = $this->attachmentService->getAllAttachmentByProcessId($id);
+
+        try {
+            return new GlobalResource(['error' => false, 'message' => $attachment], 200);
+        }catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
         }
     }
 }
