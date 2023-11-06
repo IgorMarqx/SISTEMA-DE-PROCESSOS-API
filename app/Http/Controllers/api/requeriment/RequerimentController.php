74e5dd84f7e7800a3e2fe3295f17ecf48277572b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-06 20:47:12 -0300

    feat: implement new methos on requeriment controller

--- app/Http/Controllers/api/requeriment/RequerimentController.php
+++ app/Http/Controllers/api/requeriment/RequerimentController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\api\requeriment;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\requeriment\RequerimentRequest;
+use App\Http\Resources\GlobalResource;
 use App\Services\requeriment\RequerimentService;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class RequerimentController extends Controller
@@ -14,28 +18,53 @@ class RequerimentController extends Controller
     {
         $this->requerimentService = $requerimentService;
     }
+
     /**
      * Display a listing of the resource.
+     * @throws Exception
      */
-    public function index()
+    public function index(): GlobalResource
     {
-        //
+        $requeriment = $this->requerimentService->getAll();
+
+        try {
+            return new GlobalResource(['error' => false, 'message' => $requeriment], 200);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     }
 
     /**
      * Store a newly created resource in storage.
+     * @throws Exception
      */
-    public function store(Request $request)
+    public function store(RequerimentRequest $request): GlobalResource
     {
-        //
+        $this->requerimentService->createRequeriment((array)$request);
+        try {
+            return new GlobalResource(['error' => false, 'message' => 'Requeriment created succesfully'], 201);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     }
 
     /**
      * Display the specified resource.
+     * @throws Exception
      */
-    public function show(string $id)
+    public function show(string $id): GlobalResource|null
     {
-        //
+        $requeriment = $this->requerimentService->getRequerimentById($id);
+
+        if (!$requeriment) {
+            return new GlobalResource(['error' => true, 'message' => 'Requeriment not found'], 404);
+        }
+
+        try {
+            return new GlobalResource(['error' => false, 'message' => $requeriment], 200);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     }
 
     /**
